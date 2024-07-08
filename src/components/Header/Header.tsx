14824f12c305821a1c442fd9aa1797edc5c1b7b6
francomd <francomdalmasso@gmail.com>
2024-07-08 14:42:29 -0300

    Add github link to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import ConnectButton from '@/components/ConnectButton';
-import { SHeader, SHeaderText } from '@/components/Header/styles';
+import { SHeader, SHeaderText, SLink } from '@/components/Header/styles';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <SHeader>
       <SHeaderText>
-        <span>Wonderland Challenge</span>
+        <SLink
+          href="https://github.com/francomd/wonderland-challenge"
+          target="_blank"
+        >
+          <Image
+            src="/assets/github.svg"
+            width={16}
+            height={16}
+            alt="Github icon"
+          />
+          <span>Wonderland Challenge</span>
+        </SLink>
         <small>By @0xFrann</small>
       </SHeaderText>
       <ConnectButton />
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Theme, withTheme } from '@/providers/ThemeProvider';
+import { Theme, breakpoint, withTheme } from '@/providers/ThemeProvider';
 import { styled } from '@linaria/react';
 
 export const SHeader = withTheme(styled.header<Theme>`
@@ -16,5 +16,30 @@ export const SHeaderText = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 0.5rem;
+  gap: 0.25rem;
 `;
+
+export const SLink = withTheme(styled.a<Theme>`
+  color: ${({ theme }) => theme.color.text};
+  line-height: 1;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  text-decoration: none;
+
+  &:hover,
+  &:focus,
+  &:active {
+    text-decoration: underline;
+  }
+
+  ${breakpoint('sm',
+    `
+    font-size: 0.875rem;
+    
+    img {
+      width: 24px;
+      height: 24px;
+    }
+  `)}
+`);
